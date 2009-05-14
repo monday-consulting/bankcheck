0e7f0332e153c4ead70f8435b51167dd4bfb1abc
Sascha Dömer <sdo@lmis.de>
2009-05-14 09:26:16 +0000

    Added missing tests
    git-svn-id: http://javaforge.com/svn/bankcheck/trunk@60 8ac0f7ae-2001-0010-8ab7-a7da3dd19481

--- accountvalidator/java/tests/hx/bankcheck/accountvalidator/AccountCheckTestSuite.java
+++ accountvalidator/java/tests/hx/bankcheck/accountvalidator/AccountCheckTestSuite.java
@@ -1,161 +1,57 @@
 package hx.bankcheck.accountvalidator;
 
-import hx.bankcheck.accountvalidator.impl.Checksum00Test;
-import hx.bankcheck.accountvalidator.impl.Checksum01Test;
-import hx.bankcheck.accountvalidator.impl.Checksum02Test;
-import hx.bankcheck.accountvalidator.impl.Checksum03Test;
-import hx.bankcheck.accountvalidator.impl.Checksum04Test;
-import hx.bankcheck.accountvalidator.impl.Checksum05Test;
-import hx.bankcheck.accountvalidator.impl.Checksum06Test;
-import hx.bankcheck.accountvalidator.impl.Checksum07Test;
-import hx.bankcheck.accountvalidator.impl.Checksum08Test;
-import hx.bankcheck.accountvalidator.impl.Checksum09Test;
-import hx.bankcheck.accountvalidator.impl.Checksum10Test;
-import hx.bankcheck.accountvalidator.impl.Checksum11Test;
-import hx.bankcheck.accountvalidator.impl.Checksum13Test;
-import hx.bankcheck.accountvalidator.impl.Checksum14Test;
-import hx.bankcheck.accountvalidator.impl.Checksum15Test;
-import hx.bankcheck.accountvalidator.impl.Checksum16Test;
-import hx.bankcheck.accountvalidator.impl.Checksum17Test;
-import hx.bankcheck.accountvalidator.impl.Checksum18Test;
-import hx.bankcheck.accountvalidator.impl.Checksum19Test;
-import hx.bankcheck.accountvalidator.impl.Checksum20Test;
-import hx.bankcheck.accountvalidator.impl.Checksum21Test;
-import hx.bankcheck.accountvalidator.impl.Checksum22Test;
-import hx.bankcheck.accountvalidator.impl.Checksum23Test;
-import hx.bankcheck.accountvalidator.impl.Checksum24Test;
-import hx.bankcheck.accountvalidator.impl.Checksum25Test;
-import hx.bankcheck.accountvalidator.impl.Checksum26Test;
-import hx.bankcheck.accountvalidator.impl.Checksum27Test;
-import hx.bankcheck.accountvalidator.impl.Checksum29Test;
-import hx.bankcheck.accountvalidator.impl.Checksum32Test;
-import hx.bankcheck.accountvalidator.impl.Checksum33Test;
-import hx.bankcheck.accountvalidator.impl.Checksum51Test;
-import hx.bankcheck.accountvalidator.impl.Checksum52Test;
-import hx.bankcheck.accountvalidator.impl.Checksum53Test;
-import hx.bankcheck.accountvalidator.impl.Checksum75Test;
-import hx.bankcheck.accountvalidator.impl.Checksum90Test;
-import hx.bankcheck.accountvalidator.impl.Checksum91Test;
-import hx.bankcheck.accountvalidator.impl.Checksum92Test;
-import hx.bankcheck.accountvalidator.impl.Checksum93Test;
-import hx.bankcheck.accountvalidator.impl.Checksum94Test;
-import hx.bankcheck.accountvalidator.impl.Checksum95Test;
-import hx.bankcheck.accountvalidator.impl.Checksum96Test;
-import hx.bankcheck.accountvalidator.impl.Checksum97Test;
-import hx.bankcheck.accountvalidator.impl.Checksum98Test;
-import hx.bankcheck.accountvalidator.impl.Checksum99Test;
-import hx.bankcheck.accountvalidator.impl.ChecksumA0Test;
-import hx.bankcheck.accountvalidator.impl.ChecksumA1Test;
-import hx.bankcheck.accountvalidator.impl.ChecksumA2Test;
-import hx.bankcheck.accountvalidator.impl.ChecksumA3Test;
-import hx.bankcheck.accountvalidator.impl.ChecksumA4Test;
-import hx.bankcheck.accountvalidator.impl.ChecksumA5Test;
-import hx.bankcheck.accountvalidator.impl.ChecksumA6Test;
-import hx.bankcheck.accountvalidator.impl.ChecksumA7Test;
-import hx.bankcheck.accountvalidator.impl.ChecksumA8Test;
-import hx.bankcheck.accountvalidator.impl.ChecksumA9Test;
-import hx.bankcheck.accountvalidator.impl.ChecksumB0Test;
-import hx.bankcheck.accountvalidator.impl.ChecksumB1Test;
-import hx.bankcheck.accountvalidator.impl.ChecksumB2Test;
-import hx.bankcheck.accountvalidator.impl.ChecksumB3Test;
-import hx.bankcheck.accountvalidator.impl.ChecksumB4Test;
-import hx.bankcheck.accountvalidator.impl.ChecksumB5Test;
-import hx.bankcheck.accountvalidator.impl.ChecksumB6Test;
-import hx.bankcheck.accountvalidator.impl.ChecksumB7Test;
-import hx.bankcheck.accountvalidator.impl.ChecksumB8Test;
-import hx.bankcheck.accountvalidator.impl.ChecksumB9Test;
-import hx.bankcheck.accountvalidator.impl.ChecksumC0Test;
-import hx.bankcheck.accountvalidator.impl.ChecksumC1Test;
-import hx.bankcheck.accountvalidator.impl.ChecksumC2Test;
-import hx.bankcheck.accountvalidator.impl.ChecksumC3Test;
-import hx.bankcheck.accountvalidator.impl.ChecksumC4Test;
-import hx.bankcheck.accountvalidator.impl.ChecksumC5Test;
-import hx.bankcheck.accountvalidator.impl.ChecksumC6Test;
+import hx.bankcheck.accountvalidator.impl.*;
 
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({
-	Checksum00Test.class,
-	Checksum01Test.class,
-	Checksum02Test.class,
-	Checksum03Test.class,
-	Checksum04Test.class,
-	Checksum05Test.class,
-	Checksum06Test.class,
-	Checksum07Test.class,
-	Checksum08Test.class,
-	Checksum09Test.class,
-	Checksum10Test.class,
-	Checksum11Test.class,
-	Checksum13Test.class,
-	Checksum14Test.class,
-	Checksum15Test.class,
-	Checksum16Test.class,
-	Checksum17Test.class,
-	Checksum18Test.class,
-	Checksum19Test.class,
-	Checksum20Test.class,
-	Checksum21Test.class,
-	Checksum22Test.class,
-	Checksum23Test.class,
-	Checksum24Test.class,
-	Checksum25Test.class,
-	Checksum26Test.class,
-	Checksum27Test.class,
-
-	Checksum29Test.class,
-	
-	Checksum32Test.class,
-	Checksum33Test.class,
-	
-	Checksum51Test.class,
-	Checksum52Test.class,
-	Checksum53Test.class,
-	
-	Checksum75Test.class,
-	
-	Checksum90Test.class,
-	Checksum91Test.class,
-	Checksum92Test.class,
-	Checksum93Test.class,
-	Checksum94Test.class,
-	Checksum95Test.class,
-	Checksum96Test.class,
-	Checksum97Test.class,
-	Checksum98Test.class,
-	Checksum99Test.class,
-	ChecksumA0Test.class,
-	ChecksumA1Test.class,
-	ChecksumA2Test.class,
-	ChecksumA3Test.class,
-	ChecksumA4Test.class,
-	ChecksumA5Test.class,
-	ChecksumA6Test.class,
-	ChecksumA7Test.class,
-	ChecksumA8Test.class,
-	ChecksumA9Test.class,
-	ChecksumB0Test.class,
-	ChecksumB1Test.class,
-	ChecksumB2Test.class,
-	ChecksumB3Test.class,
-	ChecksumB4Test.class,
-	ChecksumB5Test.class,
-	ChecksumB6Test.class,
-	ChecksumB7Test.class,
-	ChecksumB8Test.class,
-	ChecksumB9Test.class,
-	ChecksumC0Test.class,
-	ChecksumC1Test.class,
-	ChecksumC2Test.class,
-	ChecksumC3Test.class,
-	ChecksumC4Test.class,
-	ChecksumC5Test.class,
-	ChecksumC6Test.class
-	})
-
+@SuiteClasses( { Checksum00Test.class, Checksum01Test.class,
+		Checksum02Test.class, Checksum03Test.class, Checksum04Test.class,
+		Checksum05Test.class, Checksum06Test.class, Checksum07Test.class,
+		Checksum08Test.class, Checksum09Test.class, Checksum10Test.class,
+		Checksum11Test.class, Checksum13Test.class, Checksum14Test.class,
+		Checksum15Test.class, Checksum16Test.class, Checksum17Test.class,
+		Checksum18Test.class, Checksum19Test.class, Checksum20Test.class,
+		Checksum21Test.class, Checksum22Test.class, Checksum23Test.class,
+		Checksum24Test.class, Checksum25Test.class, Checksum26Test.class,
+		Checksum27Test.class, Checksum28Test.class, Checksum29Test.class,
+		Checksum30Test.class, Checksum31Test.class, Checksum32Test.class,
+		Checksum33Test.class, Checksum34Test.class, Checksum35Test.class,
+		Checksum36Test.class, Checksum37Test.class, Checksum38Test.class,
+		Checksum39Test.class, Checksum40Test.class, Checksum41Test.class,
+		Checksum42Test.class, Checksum43Test.class, Checksum44Test.class,
+		Checksum45Test.class, Checksum46Test.class, Checksum47Test.class,
+		Checksum48Test.class, Checksum49Test.class, Checksum50Test.class,
+		Checksum51Test.class, Checksum52Test.class, Checksum53Test.class,
+		Checksum54Test.class, Checksum55Test.class, Checksum56Test.class,
+		Checksum57Test.class, Checksum58Test.class, Checksum59Test.class,
+		Checksum60Test.class, Checksum61Test.class, Checksum62Test.class,
+		Checksum63Test.class, Checksum64Test.class, Checksum65Test.class,
+		Checksum66Test.class, Checksum67Test.class, Checksum68Test.class,
+		Checksum69Test.class, Checksum70Test.class, Checksum71Test.class,
+		Checksum72Test.class, Checksum73Test.class, Checksum74Test.class,
+		Checksum75Test.class, Checksum76Test.class, Checksum77Test.class,
+		Checksum78Test.class, Checksum79Test.class, Checksum80Test.class,
+		Checksum81Test.class, Checksum82Test.class, Checksum83Test.class,
+		Checksum84Test.class, Checksum85Test.class, Checksum86Test.class,
+		Checksum87Test.class, Checksum88Test.class, Checksum89Test.class,
+		Checksum90Test.class, Checksum91Test.class, Checksum92Test.class,
+		Checksum93Test.class, Checksum94Test.class, Checksum95Test.class,
+		Checksum96Test.class, Checksum97Test.class, Checksum98Test.class,
+		Checksum99Test.class, ChecksumA0Test.class, ChecksumA1Test.class,
+		ChecksumA2Test.class, ChecksumA3Test.class, ChecksumA4Test.class,
+		ChecksumA5Test.class, ChecksumA6Test.class, ChecksumA7Test.class,
+		ChecksumA8Test.class, ChecksumA9Test.class, ChecksumB0Test.class,
+		ChecksumB1Test.class, ChecksumB2Test.class, ChecksumB3Test.class,
+		ChecksumB4Test.class, ChecksumB5Test.class, ChecksumB6Test.class,
+		ChecksumB7Test.class, ChecksumB8Test.class, ChecksumB9Test.class,
+		ChecksumC0Test.class, ChecksumC1Test.class, ChecksumC2Test.class,
+		ChecksumC3Test.class, ChecksumC4Test.class, ChecksumC5Test.class,
+		ChecksumC6Test.class, ChecksumC7Test.class, ChecksumC8Test.class,
+		ChecksumC9Test.class, ChecksumD0Test.class, ChecksumD1Test.class,
+		ChecksumD2Test.class, ChecksumD3Test.class })
 public class AccountCheckTestSuite {
 	// Empty space
 }
--- accountvalidator/java/tests/hx/bankcheck/accountvalidator/impl/Checksum59Test.java
+++ accountvalidator/java/tests/hx/bankcheck/accountvalidator/impl/Checksum59Test.java
@@ -4,6 +4,7 @@
 package hx.bankcheck.accountvalidator.impl;
 
 import hx.bankcheck.accountvalidator.AbstractChecksumTest;
+import hx.bankcheck.accountvalidator.ChecksumValidator;
 
 /**
  * Testclass for testing algorithm 61.
@@ -21,6 +22,23 @@ public class Checksum59Test extends AbstractChecksumTest {
 	 */
 	@Override
 	public void testValidate() throws Throwable {
-		fail("Need numbers...");
+		ChecksumValidator cs = new Checksum00();
+
+		int[][] correctNumbers = new int[][] {
+				{0,0,0,9,2,9,0,7,0,1},
+				{0,5,3,9,2,9,0,8,5,8},
+				{0,0,0,1,5,0,1,8,2,4},
+				{0,0,0,1,5,0,1,8,3,2}
+		};
+		
+		assertTrue(cs.validate(correctNumbers[0],null));
+		assertTrue(cs.validate(correctNumbers[1],null));
+		assertTrue(cs.validate(correctNumbers[2],null));
+		assertTrue(cs.validate(correctNumbers[3],null));
+		
+		int[][] wrongNumbers = new int[][] {
+				{1,2,3,4,5,6,7,8,9,0}
+		};
+		assertFalse(cs.validate(wrongNumbers[0],null));
 	}
 }
