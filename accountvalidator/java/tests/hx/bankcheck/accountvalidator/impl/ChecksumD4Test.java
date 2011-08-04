2d37d57fbaa30350b8e1269455e72d7f5da5a8f7
Tobias Mayer <git@tobiasm.de>
2011-08-05 00:57:32 +0200

    Update D4

--- accountvalidator/java/tests/hx/bankcheck/accountvalidator/impl/ChecksumD4Test.java
+++ accountvalidator/java/tests/hx/bankcheck/accountvalidator/impl/ChecksumD4Test.java
@@ -5,6 +5,7 @@ package hx.bankcheck.accountvalidator.impl;
 
 import hx.bankcheck.accountvalidator.AbstractChecksumTest;
 import hx.bankcheck.accountvalidator.ChecksumValidator;
+import hx.bankcheck.accountvalidator.utils.ChecksumUtils;
 
 /**
  * Testclass for testing algorithm D4.
@@ -24,31 +25,27 @@ public class ChecksumD4Test extends AbstractChecksumTest {
 	public void testValidate() throws Throwable {
 
 		ChecksumValidator validator = new ChecksumD4();
-		
-		int[] validAccountNumber1 = { 3, 0, 0, 0, 0, 0, 5, 0, 1, 2 };
-		int[] validAccountNumber2 = { 4, 1, 4, 3, 4, 0, 6, 9, 8, 4 };
-		int[] validAccountNumber3 = { 5, 9, 2, 6, 4, 8, 5, 1, 1, 1 };
-		int[] validAccountNumber4 = { 9, 0, 0, 2, 3, 6, 4, 5, 8, 8 };
 
-		int[] invalidAccountNumber1 = { 1, 0, 0, 0, 0, 6, 2, 0, 2, 5 };
-		int[] invalidAccountNumber2 = { 0, 0, 0, 6, 0, 0, 3, 0, 2, 7 };
-		int[] invalidAccountNumber3 = { 8, 0, 0, 3, 3, 0, 6, 0, 2, 6 };
-		int[] invalidAccountNumber4 = { 9, 9, 1, 6, 5, 2, 4, 5, 3, 4 };
-		int[] invalidAccountNumber5 = { 5, 2, 1, 2, 7, 4, 4, 5, 6, 4 };
-		int[] invalidAccountNumber6 = { 3, 0, 0, 0, 2, 5, 5, 3, 9, 7 };
+		long[] validNumbers = { 1112048219L, 2024601814L, 3000005012L,
+				4143406984L, 5926485111L, 6286304975L, 7900256617L,
+				8102228628L, 9002364588L };
 
-		// Should be valid for alternative 1
-		assertTrue(validator.validate(validAccountNumber1, null));
-		assertTrue(validator.validate(validAccountNumber2, null));
-		assertTrue(validator.validate(validAccountNumber3, null));
-		assertTrue(validator.validate(validAccountNumber4, null));
+		long[] invalidNumbers = { 359432843L, 1000062023L, 2204271250L,
+				3051681017L, 4000123456L, 5212744564L, 6286420010L,
+				7859103459L, 8003306026L, 9916524534L };
 
-		// Should be invalid for alternative 1
-		assertFalse(validator.validate(invalidAccountNumber1, null));
-		assertFalse(validator.validate(invalidAccountNumber2, null));
-		assertFalse(validator.validate(invalidAccountNumber3, null));
-		assertFalse(validator.validate(invalidAccountNumber4, null));
-		assertFalse(validator.validate(invalidAccountNumber5, null));
-		assertFalse(validator.validate(invalidAccountNumber6, null));
+		// Should be valid
+		for(long n : validNumbers) {
+			int[] an = ChecksumUtils.parseAccountNumber(n);
+			boolean valid = validator.validate(an, null);
+			assertTrue(n+" is invalid!", valid);
+		}
+
+		// Should be invalid
+		for(long n : invalidNumbers) {
+			int[] an = ChecksumUtils.parseAccountNumber(n);
+			boolean valid = validator.validate(an, null);
+			assertFalse(n+" is valid!", valid);
+		}
 	}
 }
