cc0d4c7b8cbc8d5f159cee4b950197673300db9b
Dirk Schrödter <dirk.schroedter@monday-consulting.com>
2013-04-04 12:15:29 +0200

    Encoding errors fixed in the source code

--- accountvalidator/java/src/hx/bankcheck/accountvalidator/impl/ChecksumD8.java
+++ accountvalidator/java/src/hx/bankcheck/accountvalidator/impl/ChecksumD8.java
@@ -4,24 +4,24 @@ import hx.bankcheck.accountvalidator.AbstractChecksumValidator;
 import hx.bankcheck.accountvalidator.exceptions.ValidationException;
 
 /**
- * Die Kontonummer ist einschlieÃŸlich der PrÃ¼fziffer 10-stellig, ggf. ist die
- * Kontonummer fÃ¼r die PrÃ¼fzifferberechnung durch linksbÃ¼ndige AuffÃ¼llung mit
+ * Die Kontonummer ist einschließlich der Prüfziffer 10-stellig, ggf. ist die
+ * Kontonummer für die Prüfzifferberechnung durch linksbündige Auffüllung mit
  * Nullen 10-stellig darzustellen.
  * <p>
- * Die Berechnung der PrÃ¼fziffer und die mÃ¶glichen Ergebnisse richten sich nach
+ * Die Berechnung der Prüfziffer und die möglichen Ergebnisse richten sich nach
  * dem jeweils bei der entsprechenden Variante angegebenen Kontonummernkreis.
- * Entspricht eine Kontonummer keinem der vorgegebenen Kontonummern- kreise oder
- * fÃ¼hrt die Berechnung der PrÃ¼fziffer nach der Variante 1 zu einem
- * PrÃ¼fzifferfehler, so ist die Kontonummer ungÃ¼ltig.
+ * Entspricht eine Kontonummer keinem der vorgegebenen Kontonummernkreise oder
+ * führt die Berechnung der Prüfziffer nach der Variante 1 zu einem
+ * Prüfzifferfehler, so ist die Kontonummer ungültig.
  * <p>
  * <b>Variante 1:</b><br>
  * Modulus 10, Gewichtung 2, 1, 2, 1, 2, 1, 2, 1, 2<br>
- * FÃ¼r Kontonummern aus dem Kontonummernkreis 1000000000 bis 9999999999
- * entsprechen die Berechnung und mÃ¶gliche Ergebnisse der Methode 00.
+ * Für Kontonummern aus dem Kontonummernkreis 1000000000 bis 9999999999
+ * entsprechen die Berechnung und mögliche Ergebnisse der Methode 00.
  * <p>
  * <b>Variante 2:</b><br>
- * FÃ¼r den Kontonummernkreis 0010000000 bis 0099999999 gilt die Methode 09
- * (keine PrÃ¼fzifferberechnung, alle Konto- nummern sind als richtig zu werten).
+ * Für den Kontonummernkreis 0010000000 bis 0099999999 gilt die Methode 09
+ * (keine Prüfzifferberechnung, alle Kontonummern sind als richtig zu werten).
  * 
  * @author Tobias Mayer (bankcheck@tobiasm.de)
  */
